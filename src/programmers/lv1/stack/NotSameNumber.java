af103ce63b82831d6c2f4771851d019e3b84ada6
scc0103 <thdtlcks369@gmail.com>
2024-08-06 17:33:57 +0900

    같은숫자는 싫어 test case 1개 해결 필요

---
+++ src/programmers/lv1/stack/NotSameNumber.java
@@ -0,0 +1,59 @@
+package programmers.lv1.stack;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ *  LV1 Stack / Queue 같은 숫자는 싫어
+ */
+
+public class NotSameNumber {
+    public static int getArraySize(int[] arr) {
+        int size = 1;
+
+        for(int i=0; i<arr.length; i++) {
+            if(i < arr.length -1 ) {
+                if(arr[i] != arr[i+1]) {
+                    size++;
+                }
+            }else{
+                continue;
+            }
+        }
+        return size;
+    }
+
+
+    public static int[] solution(int []arr) {
+        int[] answer = new int[getArraySize(arr)];
+        int size = 0;
+        int idxAnswer = 0;
+
+        if(arr.length == 2 && arr[arr.length - 2] == arr[arr.length - 1]) {
+            answer[0] = arr[0];
+            return answer;
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            if(i < arr.length -1 ) {
+                if(arr[i] != arr[i+1]) {
+                    answer[idxAnswer] = arr[i];
+                    idxAnswer++;
+                }
+            }else {
+                if(answer[idxAnswer - 1] != arr[i]) {
+                    answer[idxAnswer] = arr[i];
+                }
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int[] arrs = {1,1,3,3,0,1,1};
+
+        System.out.println(solution(arrs));
+
+    }
+}
