d4f52245c174fe0f459c69e16c6043e1cf8e0643
scc0103 <thdtlcks369@gmail.com>
2024-10-24 22:53:49 +0900

    ãbaekjoon 17478 clear

--- src/baekjoon/N17478.java
+++ src/baekjoon/N17478.java
@@ -9,7 +9,7 @@ public class N17478 {
     static StringBuilder sb = new StringBuilder();
     static int count = 0;
     static int input = 0;
-    static String sep = "----";
+    static String sep = "____";
 
 
 
@@ -21,15 +21,20 @@ public class N17478 {
             tempSep.append(sep);
         }
 
+        // "\"
+        // \""
         sb.append(tempSep.toString());
-        sb.append("재귀함수가 뭔가요?\n");
+        sb.append("\"재귀함수가 뭔가요?\"");
+        sb.append("\n");
         sb.append(tempSep.toString());
-        sb.append("잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+        sb.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+        sb.append("\n");
         sb.append(tempSep.toString());
-        sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+        sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+        sb.append("\n");
         sb.append(tempSep.toString());
-        sb.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\n");
-        //System.out.println(sb.toString());
+        sb.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+        sb.append("\n");
 
         count = count + 1;
 
@@ -38,16 +43,21 @@ public class N17478 {
         }else {
             tempSep.append(sep);
             sb.append(tempSep.toString());
-            sb.append("재귀함수가 뭔가요?\n");
+            sb.append("\"재귀함수가 뭔가요?\"");
+            sb.append("\n");
             sb.append(tempSep.toString());
-            sb.append("재귀함수는 자기 자신을 호출하는 함수라네\n");
+            sb.append("\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            sb.append("\n");
 
             sb.append(tempSep.toString());
-            sb.append("라고 답변하였지");
+            sb.append("라고 답변하였지.");
+            sb.append("\n");
 
-            for(int i=1; i<input; i++){
+            for(int i=0; i<input; i++){
                 // seq 빼줘야함
-                //tempSep.
+                tempSep.delete(tempSep.length()-4, tempSep.length());
+                sb.append(tempSep.toString());
+                sb.append("라고 답변하였지.\n");
             }
         }
 
@@ -58,13 +68,8 @@ public class N17478 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         input  = sc.nextInt();
-
-        //count = input;
-
         sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
         solution(count);
-
-
 
         System.out.println(sb.toString());
     }
